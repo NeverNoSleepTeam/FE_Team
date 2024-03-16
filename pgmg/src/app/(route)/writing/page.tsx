ea488944c38bd78cc7f1b38e0aeb2f45854024ca
areumYang <yangareum1818@nate.com>
2024-03-16 16:31:00 +0900

    desgin : 글쓰기 calendar - react-calendar 설치 후, 마크업

--- pgmg/src/app/(route)/writing/page.tsx
+++ pgmg/src/app/(route)/writing/page.tsx
@@ -1,9 +1,16 @@
+'use client';
+
 import QuillNoSSR from '@/app/(route)/_component/Editor';
-import style from './page.module.scss';
+import style from '@/app/(route)/writing/page.module.scss';
 import Image from 'next/image';
 import images from '@/app/common/img/add-photo-alternate.png';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { useState } from 'react';
 
-export default function writing() {
+export default function Writing() {
+	const [value, onChange] = useState(new Date());
+
 	return (
 		<form className={style.formContainer}>
 			<div className={style.inputBox}>
@@ -26,10 +33,31 @@ export default function writing() {
 					<option value="etc">기타</option>
 				</select>
 			</div>
-			<div className={style.inputBoxDate}>
+			<div className={style.dataTimeWrap}>
 				<label htmlFor="" className={style.label}>
 					날짜/시간
 				</label>
+				<div className={style.dataTimeInner}>
+					<div className={style.dataTime}>
+						<div className={style.calendarBox}>
+							<input className={style.calendar} type="text" />
+							<Calendar />
+						</div>
+						<div className={style.timeBox}>
+							<input type="text" />
+						</div>
+					</div>
+					<span className={style.wave}>~</span>
+					<div className={style.dataTime}>
+						<div className={style.calendarBox}>
+							<input className={style.calendar} type="text" />
+							<Calendar />
+						</div>
+						<div className={style.timeBox}>
+							<input type="text" />
+						</div>
+					</div>
+				</div>
 			</div>
 			<div className={style.inputBox}>
 				<label htmlFor="" className={style.label}>
@@ -51,22 +79,24 @@ export default function writing() {
 				<label htmlFor="" className={style.label}>
 					내용
 				</label>
-				<div>
-					<QuillNoSSR
-						className={style.editorStyle}
-						theme="snow"
-						placeholder="상세내용을 입력해주세요."
-					/>
-				</div>
+				<QuillNoSSR
+					className={style.editorStyle}
+					theme="snow"
+					placeholder="상세내용을 입력해주세요."
+				/>
 			</div>
 			<div className={style.inputWrap}>
 				<div className={style.inputBox}>
 					<div className={style.label}>메인이미지 등록(필수)</div>
-					<Image src={images} alt="메인 이미지" />
+					<div className={style.imageBox}>
+						<Image src={images} alt="메인 이미지" />
+					</div>
 				</div>
 				<div className={style.inputBox}>
 					<div className={style.label}>상세이미지 등록</div>
-					<Image src={images} alt="상세 이미지" />
+					<div className={style.imageBox}>
+						<Image src={images} alt="상세 이미지" />
+					</div>
 				</div>
 			</div>
 		</form>

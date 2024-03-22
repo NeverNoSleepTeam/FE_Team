8c65bc0226a3fcf2dee4b53944a758de21e6d241
areumYang <yangareum1818@nate.com>
2024-03-22 17:26:03 +0900

    desgin : pofile 컴포넌트화, 게시글상세조회 폴더 생성

---
+++ pgmg/src/app/(route)/post/[postId]/page.tsx
@@ -0,0 +1,10 @@
+import { PostProfileSyle } from '../../_component/ProfileStyle';
+
+export default function PostPage() {
+	return (
+		<>
+			<div>게시글 상세페이지</div>
+			<PostProfileSyle />
+		</>
+	);
+}
